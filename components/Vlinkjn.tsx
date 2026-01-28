72471e9fde899798ff6b855a09771ec70d111831
akbai115 <azzaanbrown@gmail.com>
2026-01-28 06:59:52 -0800

    chore: remove all flame video assets from Vlinkjn

--- components/Vlinkjn.tsx
+++ components/Vlinkjn.tsx
@@ -1,16 +1,3 @@
 export const Vlinkjn: React.FC = () => {
-    return (
-        <div
-            className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[1000] pointer-events-none"
-        >
-            <video
-                src="/flames32.mp4"
-                autoPlay
-                loop
-                muted
-                playsInline
-                className="w-64 h-auto md:w-96 drop-shadow-2xl opacity-90 relative z-20"
-            />
-        </div>
-    );
+    return null;
 };
